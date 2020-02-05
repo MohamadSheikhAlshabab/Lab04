272727acaf7f22bad54eb1c009374c272ad01cf4
Your Name In Single Quotes <your email here in single quotes>
2020-02-05 09:22:27 +0200

    link js with html

---
+++ JS/lab06.js
@@ -0,0 +1,12 @@
+var your_name , your_age;
+
+your_name = prompt ("How are you?");
+//  if (   prompt == "fine" ){
+your_age = prompt("whats your name?");
+//  }
+confirm(your_name + "can you write again your age,PLZ?");
+confirm(your_name + "can you write again your name , PLZ ?");
+
+document.write('<h3>' +"your_name is :" + your_name + '</h3>')
+document.write('<h4>' + "your age is :" + your_age + '</h4>')
+prompt("muhammad");
