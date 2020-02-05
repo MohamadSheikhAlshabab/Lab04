54158f638e9ba9b7357e69d953e17f879de04e60
Your Name In Single Quotes <your email here in single quotes>
2020-02-05 22:02:24 +0200

    edit

--- JS/lab06.js
+++ JS/lab06.js
@@ -1,12 +1,23 @@
 var your_name , your_age;
 
-your_name = prompt ("How are you?");
-//  if (   prompt == "fine" ){
-your_age = prompt("whats your name?");
-//  }
-confirm(your_name + "can you write again your age,PLZ?");
-confirm(your_name + "can you write again your name , PLZ ?");
+your_name = prompt ("what is your name ? ");
+ if (   your_name == "mohamad" ){
+    
+    confirm(your_name + ", " +"can you write again your age,PLZ?");
+}
 
-document.write('<h3>' +"your_name is :" + your_name + '</h3>')
-document.write('<h4>' + "your age is :" + your_age + '</h4>')
-prompt("muhammad");
+document.write('<h3>' +"your_name is :" + your_name + '</h3>');
+your_age = prompt("whats your grade ?");
+if (your_age >=90 || your_age <=100){
+    document.write('<h4>' + "your grade is :" + your_age + '</h4>');
+} else if (your_age >=80 || your_age <=89){
+    document.write('<h4>' + "your grade is :" + your_age + '</h4>');
+} else if (your_age >=70 || your_age <=79){
+    document.write('<h4>' + "your grade is :" + your_age + '</h4>');
+} else {
+    document.write('<h4>' + "your grade is :" + " F A I L" + '</h4>');
+} 
+
+
+
+alert("T H A N K Y O U" + " " + your_name);
