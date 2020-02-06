b53ee5b7a827a866052243ba052237d6e515f8cf
Your Name In Single Quotes <your email here in single quotes>
2020-02-06 08:48:18 +0200

    saas

--- JS/lab06.js
+++ JS/lab06.js
@@ -1,22 +1,51 @@
-var your_name , your_age;
+var your_name , your_age ;
+var p1 ;
+var  p2 ;
+var sum;
 
 your_name = prompt ("what is your name ? ");
- if (   your_name == "mohamad" ){
+p1 =prompt("Input first Number");
+
+p2 =prompt("Input second Number");
+//  if (   your_name == "mohamad" ){
     
-    confirm(your_name + ", " +"can you write again your age,PLZ?");
+confirm("HELLO "+ "," +your_name );
+while( p1 == 1 &&  p2 == 1){
+    p1 =prompt("Input first Number, should not equal 1");
+
+    p2 =prompt("Input second Number, should not equal 1");
+    
 }
 
+
+var num=function multi(){
+
+    // if (p1 ===Number &&  p2 === Number ){continue;}
+    
+    sum=p1*p2;
+    return sum;
+}
+num();
+
+// }
+
 document.write('<h3>' +"your_name is :" + your_name + '</h3>');
-your_age = prompt("whats your grade ?");
-if (your_age >=90 || your_age <=100){
-    document.write('<h4>' + "your grade is :" + your_age + '</h4>');
-} else if (your_age >=80 || your_age <=89){
-    document.write('<h4>' + "your grade is :" + your_age + '</h4>');
-} else if (your_age >=70 || your_age <=79){
-    document.write('<h4>' + "your grade is :" + your_age + '</h4>');
-} else {
-    document.write('<h4>' + "your grade is :" + " F A I L" + '</h4>');
-} 
+document.write("sum is =" + "  " + sum);
+// your_age = prompt("what is your drink ?");
+// while (your_age !== "coffee" || your_age !== "Tea"){
+//     your_age =prompt("Choice coffee or Tea");
+//     return your_age;
+// }
+// // 
+// if (your_age >=90 || your_age <=100){
+//     document.write('<h4>' + "your grade is :" + your_age + '</h4>');
+// } else if (your_age >=80 || your_age <=89){
+//     document.write('<h4>' + "your grade is :" + your_age + '</h4>');
+// } else if (your_age >=70 || your_age <=79){
+//     document.write('<h4>' + "your grade is :" + your_age + '</h4>');
+// } else {
+//     document.write('<h4>' + "your grade is :" + " F A I L" + '</h4>');
+// } 
 
 
 
